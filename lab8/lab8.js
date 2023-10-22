e053beec1ebb80a800574405c2e688da00029be9
opem12 <100376126+opem12@users.noreply.github.com>
2023-10-22 16:50:45 -0500

    Update lab8.js

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,4 +1,29 @@
-
 function getDataFromForm() {
-  alert("it worked!");
+  const firstName = document.getElementById("fname").value;
+  const lastName = document.getElementById("lname").value;
+
+  runAjax(firstName, lastName);
+}
+
+function runAjax(fname, lname) {
+  const xhr = new XMLHttpRequest();
+
+
+  const url = `./ajax.php?firstname=${fname}&lastname=${lname}`;
+
+  xhr.open("GET", url, true);
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+
+      if (typeof xhr.responseText === "string") {
+        document.getElementById("responseString").textContent = xhr.responseText;
+      }
+{
+      }
+      
+    }
+  };
+
+  xhr.send();
 }
