2bdd0cfd88a29b69f3a15dd631493d1f832ffcdd
ope <100376126+opem12@users.noreply.github.com>
2023-11-05 15:00:28 -0600

    Update save_data.php

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -1,29 +1,18 @@
 <?php
-// Check if the post request contains first and last name
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['first_name']) && isset($_POST['last_name'])) {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-
-    // Create and append data to a file in the private directory
-    $data = "$first_name,$last_name\n";
-    $filename = '/var/www/html/opeyemivxp.azurewebsites.net/non-public-folder/data.txt';
-
-    // Check if the directory exists
-    if (!is_dir('private')) {
-        mkdir('private');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $firstName = $_POST['first_name'];
+    $lastName = $_POST['last_name'];
+    $data = $firstName . ' ' . $lastName . "\n";
+    
+    $filePath = '/var/www/html/opeyemivxp.azurewebsites.net/non-public-folder/data.txt'; // Define the path to your non-public folder.
+    
+    // Append data to the file.
+    if (file_put_contents($filePath, $data, FILE_APPEND | LOCK_EX) !== false) {
+        echo 'Data saved successfully.';
+    } else {
+        echo 'Error saving data.';
     }
-
-    // Open the file for appending
-    $file = fopen($filename, 'a');
-
-    // Write the data to the file
-    fwrite($file, $data);
-
-    // Close the file
-    fclose($file);
-
-    echo "Data saved to $filename";
 } else {
-    echo "Error: Missing first and last name in the POST request.";
+    echo 'Invalid request.';
 }
 ?>
