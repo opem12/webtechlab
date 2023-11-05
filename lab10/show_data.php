c40f5da7f49bc5a40813c179c4035c8db50b9405
ope <100376126+opem12@users.noreply.github.com>
2023-11-05 14:28:22 -0600

    Update show_data.php

--- lab10/show_data.php
+++ lab10/show_data.php
@@ -1,5 +1,5 @@
 <?php
-$filePath = 'non-public-folder/data.txt'; // Define the path to your non-public folder.
+$filePath = 'data/data.txt'; // Define the path to your non-public folder.
 if (file_exists($filePath)) {
     $fileContent = file($filePath, FILE_IGNORE_NEW_LINES);
     if (!empty($fileContent)) {
