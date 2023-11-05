9da5e4ff24217344052405597f5912ea67be8953
ope <100376126+opem12@users.noreply.github.com>
2023-11-05 14:28:11 -0600

    Update save_data.php

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $lastName = $_POST['last_name'];
     $data = $firstName . ' ' . $lastName . "\n";
     
-    $filePath = 'non-public-folder/data.txt'; // Define the path to your non-public folder.
+    $filePath = 'data/data.txt'; // Define the path to your non-public folder.
     
     // Append data to the file.
     if (file_put_contents($filePath, $data, FILE_APPEND | LOCK_EX) !== false) {
