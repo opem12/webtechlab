4cad553b76da8216c76e6450f9188b071f8ac168
ope <100376126+opem12@users.noreply.github.com>
2023-11-05 14:31:30 -0600

    Update show_data.php

--- lab10/show_data.php
+++ lab10/show_data.php
@@ -1,18 +1,26 @@
 <?php
-$filePath = 'data/data.txt'; // Define the path to your non-public folder.
-if (file_exists($filePath)) {
-    $fileContent = file($filePath, FILE_IGNORE_NEW_LINES);
-    if (!empty($fileContent)) {
-        echo '<table border="1">';
-        echo '<tr><th>Full Name</th></tr>';
-        foreach ($fileContent as $line) {
-            echo '<tr><td>' . htmlspecialchars($line) . '</td></tr>';
-        }
-        echo '</table>';
-    } else {
-        echo 'No data available.';
+// Read the contents of the user_data.txt file
+$data = file_get_contents('private/user_data.txt');
+
+// Check if the file contains data
+if ($data) {
+    // Split the data into an array of lines
+    $lines = explode("\n", $data);
+
+    // Display the data in a table format
+    echo "<table>";
+    echo "<tr><th>First Name</th><th>Last Name</th></tr>";
+
+    foreach ($lines as $line) {
+        $parts = explode(",", $line);
+        $firstName = $parts[0];
+        $lastName = $parts[1];
+
+        echo "<tr><td>$firstName</td><td>$lastName</td></tr>";
     }
+
+    echo "</table>";
 } else {
-    echo 'Data file not found.';
+    echo "No data found in the user_data.txt file.";
 }
 ?>
