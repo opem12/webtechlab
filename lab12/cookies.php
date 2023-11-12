1aa2f972c1384e5b2b052aba16e7b3a500b468cc
ope <100376126+opem12@users.noreply.github.com>
2023-11-12 15:20:38 -0600

    Update cookies.php

--- lab12/cookies.php
+++ lab12/cookies.php
@@ -2,7 +2,7 @@
 
 
 if (isset($_COOKIE['cs4413'])) {
-    echo "The cookie 'cs4413' is already set with the value: " . $_COOKIE['cs4413'];
+    echo " cookie 'cs4413' is already set value - " . $_COOKIE['cs4413'];
 } else {
     
     $cookieValue = "cs4413"; 
@@ -10,7 +10,7 @@ if (isset($_COOKIE['cs4413'])) {
 
     setcookie('cs4413', $cookieValue, $expirationTime);
 
-    echo "The cookie 'cs4413' has been set with the value: " . $cookieValue;
+    echo "cookie 'cs4413' has been set value - " . $cookieValue;
 }
 
 ?>
